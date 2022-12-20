3cba1d82605a113df01f20283d613e94ba8f6451
Mathieu Laversin <mathieulaversin @outlook.fr>
2022-12-20 13:40:23 +0100

    Tp2-commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+import requests
 
 app = Flask(__name__)
+g_analytics_url = 'https://analytics.google.com/analytics/web/#/p344233666/reports/intelligenthome'
 
 @app.route('/', methods=["GET"])
-def hello_world():
-    return "Hola world"
+def index():
+  return render_template('index.html')
+
+@app.route('/logs/', methods=["GET"])
+def show_logs():
+  return render_template('logs.html')
+
+
+
+@app.route('/cookie/', methods=["GET"])
+def cookie(): 
+  return requests.get(g_analytics_url).text 
+
